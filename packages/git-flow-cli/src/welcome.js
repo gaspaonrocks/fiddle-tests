e4304a6b35e2ecb0956afdb4d9467612aff53a49
Sacha GRASSO <sacha.grasso-ext@atinternet.com>
2019-06-23 14:49:23 +0200

    return to main menu done !

--- packages/git-flow-cli/src/welcome.js
+++ packages/git-flow-cli/src/welcome.js
@@ -15,34 +15,66 @@ const forked = fork('./packages/git-flow-cli/src/fork.js');
 
 readline.emitKeypressEvents(process.stdin);
 
+const removeKeyPressListeners = () => {
+    process.stdin.removeAllListeners('keypress');
+}
+
 /**
  * @todo: externalise it.
  */
 process.stdin.setEncoding('utf8');
 process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-    if (key.ctrl && key.name === 'c') {
-        process.stdout.write('\x1Bc');
-        process.exit();
-    } else {
-        process.stdout.write('\x1Bc');
-        // console.log(key);
 
-        if (key.name === 'up') {
-            forked.send({ message: "What do you want to do ?", data: makeMyMessage(shiftArray(choices)), execute: false });
-        } else if (key.name === 'down') {
-            forked.send({ message: "What do you want to do ?", data: makeMyMessage(unshiftArray(choices)), execute: false });
-        } else if (key.name === 'escape') {
+const statusSelect = (choice) => {
+    removeKeyPressListeners();
+    process.stdout.write('\x1Bc');
+    forked.send({ message: `You selected ${choices[0]}`, data: choices[0], execute: true });
+    process.stdin.on('keypress', (str, key) => {
+        if (key.ctrl && key.name === 'c') {
             process.stdout.write('\x1Bc');
             process.exit();
-        } else if (key.name === 'return') {
-            process.stdout.write('\x1Bc');
-            forked.send({ message: `You selected ${choices[0]}`, data: choices[0], execute: true });
-        }
-    }
-});
+        } else {
+            // console.log(key);
+
+            if (key.name === 'g') {
+                // forked.send({ message: "What do you want to do ?", data: makeMyMessage(shiftArray(choices)), execute: false });
+                process.stdout.write('\x1Bc');
+                selectAction();
+            };
+        };
+    })
+}
 
 const selectAction = () => {
+    removeKeyPressListeners();
+    process.stdout.write('\x1Bc');
+    process.stdin.on('keypress', (str, key) => {
+        if (key.ctrl && key.name === 'c') {
+            process.stdout.write('\x1Bc');
+            process.exit();
+        } else {
+            /**
+             * @todo mapper des action spécifiques avec des touches spécifiques.
+             */
+            // console.log(key);
+
+            if (key.name === 'up') {
+                process.stdout.write('\x1Bc');
+                forked.send({ message: "What do you want to do ?", data: makeMyMessage(shiftArray(choices)), execute: false });
+            } else if (key.name === 'down') {
+                process.stdout.write('\x1Bc');
+                forked.send({ message: "What do you want to do ?", data: makeMyMessage(unshiftArray(choices)), execute: false });
+            } else if (key.name === 'escape') {
+                process.stdout.write('\x1Bc');
+                process.exit();
+            } else if (key.name === 'return') {
+                // forked.send({ message: `You selected ${choices[0]}`, data: choices[0], execute: true });
+                statusSelect(choices[0]);
+                // process.exit(); ?
+            }
+        }
+    });
+
     process.stdout.write('\x1Bc');
     forked.send({ message: "What do you want to do ?", data: makeMyMessage(choices) });
 }
